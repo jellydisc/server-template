efb5a0ec29a885e51c85e97a437d61f9511f5111
jellydisc <jellydiscdev@gmail.com>
2021-08-23 11:46:24 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// These import necessary modules and set some initial variables
+require("dotenv").config();
+const express = require("express");
+const fetch = require("node-fetch");
+const convert = require("xml-js");
+const rateLimit = require("express-rate-limit");
+var cors = require("cors");
+const app = express();
+const port = 3080;
+
+// Rate limiting - Goodreads limits to 1/sec, so we should too
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: 1000, // 1 second
+  max: 1, // limit each IP to 1 requests per windowMs
+});
+
+//  apply to all requests
+app.use(limiter);
+
+// Allow CORS from any origin
+app.use(cors());
+
+// Routes
+
+// Test route, visit localhost:3000 to confirm it's working
+// should show 'Hello World!' in the browser
+app.get("/", (req, res) => res.send("Hello World!"));
+
+// This spins up our sever and generates logs for us to use.
+// Any console.log statements you use in node for debugging will show up in your
+// terminal, not in the browser console!
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
